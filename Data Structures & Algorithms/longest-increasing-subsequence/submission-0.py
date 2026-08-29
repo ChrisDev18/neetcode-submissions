6f1394563883b311dcdcbeb9b1d06e48b53bc1cf
Chris W <95181085+ChrisDev18@users.noreply.github.com>
2026-08-29 19:02:49 +0100

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # opt[i] represents the longest subsequence that ends at nums[i]
+        opt = [1 for i in range(len(nums))]
+        
+        # Each iteration will check possible solutions
+        # that could include elements up until nums[i]
+        for i in range(1, len(nums)):
+            # build up opt[i] from all previous opt values opt[j]
+            for j in range(0, i):
+                # you can only add to subsequence if nums[i] > nums[j]:
+                if nums[i] > nums[j]:
+                    # opt stores optimal length so assign the max value
+                    opt[i] = max(opt[i], opt[j] + 1)
+        
+        return max(opt)
